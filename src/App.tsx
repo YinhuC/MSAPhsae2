33eef8248ec27b123167f99913c68401b4ca086f
YinhuC <yinhucui@hotmail.com>
2018-11-21 22:59:27 +1300

    Template, UI complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,84 @@
 import * as React from 'react';
 import './App.css';
-
-import logo from './logo.svg';
+import { Header } from './Components/Header';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import UpIcon from '@material-ui/icons/KeyboardArrowUp';
+import DownIcon from '@material-ui/icons/KeyboardArrowDown';
 
 class App extends React.Component {
+
+
+
   public render() {
+
+    const temp: string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos modi odio, sapiente consequuntur" +
+      "pariatur quo, illumww eius, et www sunt assumenda dicta non reiciendis exercitationem molestiae?" +
+      "A totam magnam libero? "
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <Header />
+
+        <Grid className="Grid" xs={8}>
+          <Paper className="Paper">
+            {temp}<br />
+            <p className="Vote">
+            <IconButton aria-label="UpVote">
+              <UpIcon />
+            </IconButton>
+            UpVotes: <br />
+            </p>
+            <p className="Vote">
+            <IconButton aria-label="DownVote">
+              <DownIcon />
+            </IconButton>
+            DownVotes:
+            </p>
+          </Paper>
+        </Grid>
+
+        <Grid className="Grid" xs={8}>
+          <Paper className="Paper">
+            {temp}<br />
+            <p className="Vote">
+            <IconButton aria-label="UpVote">
+              <UpIcon />
+            </IconButton>
+            UpVotes: <br />
+            </p>
+            <p className="Vote">
+            <IconButton aria-label="DownVote">
+              <DownIcon />
+            </IconButton>
+            DownVotes:
+            </p>
+          </Paper>
+        </Grid>
+
+        <Grid className="Grid" xs={8}>
+          <Paper className="Paper">
+            {temp}<br />
+            <p className="Vote">
+            <IconButton aria-label="UpVote">
+              <UpIcon />
+            </IconButton>
+            UpVotes: <br />
+            </p>
+            <p className="Vote">
+            <IconButton aria-label="DownVote">
+              <DownIcon />
+            </IconButton>
+            DownVotes:
+            </p>
+          </Paper>
+        </Grid>
+
+
+      </div >
     );
+
   }
 }
 

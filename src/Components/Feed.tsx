1855ec3a43a7349bc1470c0475ce72002fa368f1
YinhuC <yinhucui@hotmail.com>
2018-11-24 05:26:19 +1300

    mic update

--- src/Components/Feed.tsx
+++ src/Components/Feed.tsx
@@ -9,6 +9,7 @@ import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import Chatbot from './Chatbot';
 import '../CSSFiles/App.css';
+import MediaStreamRecorder from 'msr';
 
 
 interface IState {
@@ -56,6 +57,7 @@ export default class Feed extends React.Component<{}, IState> {
 
                 <div className="posting">
                     <Input
+                        id="search-tag-textbox"
                         placeholder="Enter words to search"
                         inputProps={{ 'aria-label': 'Description', }}
                         style={{ fontSize: '30px', borderWidth: 1, color: "black" }}
@@ -70,11 +72,76 @@ export default class Feed extends React.Component<{}, IState> {
                         onClick={this.handleClick}>
                         <div className="enter">Search</div>
                     </Button>
+                    <div style={{ margin: '20px' }} className="btn" onClick={this.searchTagByVoice}><i className="fa fa-microphone" /></div>
                 </div>
 
                 {this.createPosts()}
             </div>
         );
+    }
+
+    public searchTagByVoice() {
+        const mediaConstraints = {
+            audio: true,
+        }
+
+        const onMediaSuccess = (stream: any) => {
+            const mediaRecorder = new MediaStreamRecorder(stream);
+            mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
+            mediaRecorder.ondataavailable = (blob: any) => {
+                this.uploadAudio(blob);
+                mediaRecorder.stop()
+            }
+            mediaRecorder.start(3000);
+        }
+
+        navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError)
+
+        function onMediaError(e: any) {
+            console.error('media error', e);
+        }
+    }
+
+    public uploadAudio(blob: any) {
+
+        let accessToken: any;
+        fetch('https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken', {
+            headers: {
+                'Content-Length': '0',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Ocp-Apim-Subscription-Key': '1053010f56214753a8e7bfe1733d4636'
+            },
+            method: 'POST'
+        }).then((response) => {
+            console.log(response.text())
+            return response.text()
+        }).then((response) => {
+            console.log(response)
+            accessToken = response
+        }).catch((error) => {
+            console.log("Error", error)
+        });
+
+        // posting audio
+        fetch('https://westus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US', {
+            body: blob, // this is a .wav audio file    
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer' + accessToken,
+                'Content-Type': 'audio/wav;codec=audio/pcm; samplerate=16000',
+                'Ocp-Apim-Subscription-Key': '1053010f56214753a8e7bfe1733d4636'
+            },
+            method: 'POST'
+        }).then((res) => {
+            return res.json()
+        }).then((res: any) => {
+            console.log(res)
+            const textBox = document.getElementById("search-tag-textbox") as HTMLInputElement
+            textBox.value = (res.DisplayText as string).slice(0, -1)
+        }).catch((error) => {
+            console.log("Error", error)
+        });
+
     }
 
     // GET the posts on the server
